26d8b33b475e269bca1bed10128cbdd6e54c4486
zrjack137soccer <jackzcxc137@gmail.com>
2026-07-12 23:43:03 -0600

    Adding early exiting based on boolean

--- sequencer.lua
+++ sequencer.lua
@@ -17,11 +17,44 @@ local sequence_a = {
   end
 }
 
+sequence_b = {
+function(state)
+    state.health = 100
+    print("Step 1: Health is 100. Continuing.")
+    return true
+  end,
+  
+  function(state)
+    state.health = 0
+    print("Step 2: Health reached 0!")
+    
+    if state.health <= 0 then
+      print("-> Halting sequence.")
+      return false -- This tells the sequencer to stop
+    end
+    
+    return true
+  end,
+  
+  function(state)
+    -- This step is never reached
+    print("Step 3: You survived!") 
+  end
+}
+
 local sequencer = function (sequence)
   local state = {}
   for _, f in ipairs(sequence) do
-   f(state)
+    local should_continue = f(state)
+    
+    if should_continue == false then
+      break
+    end
   end
 end
 
+print("Sequence A -- simple sequence\n")
 sequencer(sequence_a)
+
+print("\nSequence B -- sequence triggering early exit\n")
+sequencer(sequence_b)
